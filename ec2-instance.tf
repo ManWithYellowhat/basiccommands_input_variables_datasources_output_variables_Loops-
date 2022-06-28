4aa8e29d0b19cd5dfe462e1a397a195c6238a87a
basinforcode <codebasin@outlook.de>
2022-06-28 18:57:08 +0200

    ec2-instance.tf

---
+++ ec2-instance.tf
@@ -0,0 +1,18 @@
+#Terraform Settings Block
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+    profile = "default"
+    region = "us-east-1"
+}
+
+resource "aws_instance" "ec2demo" {
+    ami = "ami-04d29b6f966df1537"
+    instance_type = "t2.micro"
+}
